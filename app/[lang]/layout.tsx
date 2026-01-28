2ae63cae4be1474a21e9a4419d6873dc0dd7c966
perawita <perawitayasa@gmail.com>
2026-01-29 00:24:50 +0800

    y

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -25,9 +25,9 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ lang: string }>
+  params: { lang: string };
 }) {
-  const { lang } = await params
+  const { lang } = params;
   const t = getDictionary(lang);
 
   return (
